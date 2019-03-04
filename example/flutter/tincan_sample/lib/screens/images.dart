d0c4bde612f2e8003c644973778a90b603d5e142
Joe Blough <joe.blough@gmail.com>
2019-03-04 16:34:15 -0500

    Added images from open clipart

---
+++ example/flutter/tincan_sample/lib/screens/images.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:tincan_sample/blocs/lrs_bloc.dart';
+
+class ImageSelectionScreen extends StatelessWidget {
+  final _nameRegex = RegExp('assets/images/(.*).png');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('Select an image'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 10.0,
+        padding: EdgeInsets.all(8.0),
+        children: <Widget>[
+          _imageButton('assets/images/bird.png'),
+          _imageButton('assets/images/books.png'),
+          _imageButton('assets/images/checkmark.png'),
+          _imageButton('assets/images/fire.png'),
+          _imageButton('assets/images/fish.png'),
+          _imageButton('assets/images/night-city.png'),
+          _imageButton('assets/images/pencil.png'),
+          _imageButton('assets/images/soccer-ball.png'),
+          _imageButton('assets/images/tux.png'),
+          _imageButton('assets/images/yin-yang.png'),
+        ],
+      ),
+    );
+  }
+
+  Widget _imageButton(String asset) {
+    return FlatButton(
+      child: Image.asset(asset),
+      onPressed: () => _imageSelected(asset),
+    );
+  }
+
+  Future<void> _imageSelected(String asset) async {
+    final name = _nameRegex.firstMatch(asset).group(1);
+    // Save the selection as an xAPI statement with the image as an attachment
+    final image = await rootBundle.load(asset);
+    lrsBloc.recordStatement(
+      Statement(
+        verb: Verb(
+            id: 'http://adlnet.gov/expapi/verbs/selected',
+            display: LanguageMap({'en-US': 'selected'})),
+        object: Activity(id: 'http://tincanapi.com/TinCanDart/example/images'),
+        attachments: [
+          Attachment(
+              content: AttachmentContent.fromByteData(image),
+              display: LanguageMap({'en-US': name}),
+              description: LanguageMap({'en-US': 'A picture of $name'}),
+              contentType: "image/png",
+              usageType: 'http://id.tincanapi.com/attachment/supporting_media'),
+        ],
+      ),
+    );
+  }
+}
