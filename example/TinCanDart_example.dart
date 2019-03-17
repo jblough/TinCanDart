c2a0ec57e16655f837dc4bf8465ab017dc40e148
Joe Blough <joe.blough@gmail.com>
2019-03-17 18:33:15 -0400

    Renamed angulardart example to "web".  Added web dependencies to dev dependencies.  Added content to command line example.

--- example/TinCanDart_example.dart
+++ example/TinCanDart_example.dart
@@ -1,3 +1,29 @@
-main() {
-  // TODO: Add sample library usage (possibly Flutter, AngularDart, Aqueduct)
+import 'package:tin_can/tin_can.dart';
+
+main() async {
+  final lrs = RemoteLRS(
+    endpoint: '',
+    username: '',
+    password: '',
+  );
+
+  final statements = await lrs.queryStatements(StatementsQuery(limit: 10));
+
+  print('Statement count : ${statements.data?.statements?.length}');
+  _printStatements(statements?.data?.statements);
+  print('more at ${statements.data?.moreUrl}');
+
+  String more = statements.data?.moreUrl;
+  while (more != null) {
+    _printStatements(statements?.data?.statements);
+    final moreStatements = await lrs.moreStatements(more);
+    print('more at ${moreStatements.data?.moreUrl}');
+    more = moreStatements.data?.moreUrl;
+  }
+}
+
+void _printStatements(List<Statement> statements) {
+  statements?.forEach((statement) {
+    print('  Statement - ${statement.id}');
+  });
 }
