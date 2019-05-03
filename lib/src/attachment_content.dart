bca984afab5e60dd8c3023661788a5e62cbb9b19
Joe Blough <joe.blough@gmail.com>
2019-05-03 19:27:39 -0400

    Add attachment content options for int and double

--- lib/src/attachment_content.dart
+++ lib/src/attachment_content.dart
@@ -14,6 +14,9 @@ class AttachmentContent {
   AttachmentContent.fromByteData(ByteData content)
       : _content = content.buffer.asUint8List();
 
+  AttachmentContent.fromNumber(num content)
+      : _content = _convertNumber(content);
+
   ByteBuffer asByteBuffer() => Uint8List.fromList(_content).buffer;
 
   String asString() => utf8.decode(_content);
@@ -22,5 +25,31 @@ class AttachmentContent {
 
   Uint8List asUint8List() => Uint8List.fromList(_content);
 
+  int asInt() {
+    try {
+      return ByteData.view(Uint8List.fromList(_content).buffer).getInt64(0);
+    } catch (e) {
+      return null;
+    }
+  }
+
+  double asDouble() {
+    try {
+      return ByteData.view(Uint8List.fromList(_content).buffer).getFloat64(0);
+    } catch (e) {
+      return null;
+    }
+  }
+
   int get length => _content.length;
+
+  static List<int> _convertNumber(num content) {
+    ByteData data = ByteData(8);
+    if (content is int) {
+      data.setInt64(0, content);
+    } else {
+      data.setFloat64(0, content);
+    }
+    return data.buffer.asUint8List();
+  }
 }
